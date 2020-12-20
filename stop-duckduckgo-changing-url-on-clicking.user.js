4fb26b88dacc7721423b033f925b40708b38649c
Gea-Suan Lin <gslin@gslin.org>
2020-12-20 23:16:39 +0800

    Import.

---
+++ stop-duckduckgo-changing-url-on-clicking.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name        Stop DuckDuckGo changing url on clicking
+// @namespace   https://wiki.gslin.com/wiki/StopDuckDuckGoChangingUrlOnClicking
+// @match       https://duckduckgo.com/*
+// @grant       none
+// @run-at      document-idle
+// @version     0.20201220.0
+// @license     MIT
+// @author      Gea-Suan Lin <gslin@gslin.org>
+// @description Stop DuckDuckGo changing url on clicking
+// ==/UserScript==
+
+(() => {
+    'use strict';
+
+    // Block replaceState (History API) to avoid DuckDuckGo from changing url:
+    // https://developer.mozilla.org/en-US/docs/Web/API/History_API
+    window.history.replaceState = function(){};
+})();
